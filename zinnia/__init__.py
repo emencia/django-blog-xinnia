1320c2de46a5087c3e4e571159e081a63522894c
David THENON <sveetch@gmail.com>
2021-02-23 03:36:24 +0100

    [CHG] Update package infos and changelog

--- zinnia/__init__.py
+++ zinnia/__init__.py
@@ -2,8 +2,8 @@
 __version__ = '0.20.1'
 __license__ = 'BSD License'
 
-__author__ = 'Fantomas42'
-__email__ = 'fantomas42@gmail.com'
+__author__ = 'Emencia',
+__email__ = 'support@emencia.com',
 
 __url__ = 'https://github.com/emencia/django-blog-xinnia'
 
