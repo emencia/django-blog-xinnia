efab58fd24bc544e52fda6727eea7956cc9d77e2
David THENON <sveetch@gmail.com>
2021-02-25 01:34:38 +0100

    [FIX] Release 0.21.1 to fix package publishing

--- zinnia/__init__.py
+++ zinnia/__init__.py
@@ -1,9 +1,9 @@
 """Zinnia"""
-__version__ = '0.21.0'
+__version__ = '0.21.1'
 __license__ = 'BSD License'
 
-__author__ = 'Emencia',
-__email__ = 'support@emencia.com',
+__author__ = 'Emencia'
+__email__ = 'support@emencia.com'
 
 __url__ = 'https://github.com/emencia/django-blog-xinnia'
 
